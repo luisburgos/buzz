3bedfb140ac301e81ce1d3efc5fbcacd7fe57b0d
Luis Burgos <luisarturo.burgos@gmail.com>
2022-06-24 23:16:55 -0500

    chore: remove unused classes

--- lib/buzz.dart
+++ lib/buzz.dart
@@ -1,7 +1,6 @@
 library buzz;
 
 export 'buzz_impl.dart';
-export 'infra/event_bus_facade.dart';
 export 'infra/event_bus_holder.dart';
 export 'infra/typed_event_bus.dart';
 export 'kinds/app_events.dart';
