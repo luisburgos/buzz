7817f9ee41babe326eeed0faaf199e377d78c469
Luis Burgos <luisarturo.burgos@gmail.com>
2022-08-20 20:15:51 -0500

    refactor: update navigation test files naming

--- test/navigation/navigator_test.dart
+++ test/navigation/navigator_test.dart
@@ -3,63 +3,8 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import '../fixtures/navigator.dart';
-import '../utils/test_helper.dart';
 
 void main() {
-  group('Buzz events', () {
-    _initBuzz() {
-      Buzz.init(navigator: MockNavigator());
-    }
-
-    buzzTestEmitsInOrder(
-      'NavigateToCommand emits OnNavigatedToEvent',
-      given: () async {
-        _initBuzz();
-      },
-      expectCommandsMatchers: () => [
-        predicate<NavigateToCommand>((command) {
-          expect(command.directions.routeBuilder(), '/login');
-          return true;
-        }),
-      ],
-      expectAppEventsMatchers: () => [
-        predicate<OnNavigatedToEvent>((command) {
-          expect(command.path, '/login');
-          return true;
-        }),
-      ],
-      fire: () => [
-        NavigateToCommand(
-          directions: NavigationDirections(routeBuilder: () => '/login'),
-        ),
-      ],
-    );
-
-    buzzTestEmitsInOrder(
-      'NavigateBackCommand emits OnNavigatedBackEvent',
-      given: () async {
-        _initBuzz();
-      },
-      expectCommandsMatchers: () => [
-        predicate<NavigateBackCommand>((command) {
-          expect(command.preferredBackDefault, '/home');
-          return true;
-        }),
-      ],
-      expectAppEventsMatchers: () => [
-        predicate<OnNavigatedBackEvent>((command) {
-          expect(command.fallbackPath, '/home');
-          return true;
-        }),
-      ],
-      fire: () => [
-        NavigateBackCommand(
-          preferredBackDefault: '/home',
-        ),
-      ],
-    );
-  });
-
   group('Navigator called from NavigationCommandHandler', () {
     MockNavigator mockNavigator = MockNavigator();
 
